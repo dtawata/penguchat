20a42e320b7b03d4c6e026c5634bcd4c13cb7645
Daniel <tawata.daniel@gmail.com>
2023-05-04 13:51:51 -0700

    updated sidebar css

--- components/Sidebar.js
+++ components/Sidebar.js
@@ -9,7 +9,7 @@ const Sidebar = (props) => {
   return (
     <div className={styles.container}>
       <div onClick={changeDirect} className={styles.direct}>
-        <Image className={styles.direct_img} src='/img/kier-in-sight-2iy6ohGsGAc-unsplash.jpg' alt='' width='50' height='50' />
+        <Image className={styles.direct_img} src='/img/kier-in-sight-2iy6ohGsGAc-unsplash.jpg' alt='' width='55' height='55' />
       </div>
       <div className={styles.line}></div>
       <div className={styles.rooms}>
@@ -17,8 +17,10 @@ const Sidebar = (props) => {
           return <Room room={room} changeRoom={changeRoom} key={room.id} />
         })}
       </div>
-      <FontAwesomeIcon icon={faPlus} className={styles.icon} onClick={() => { updateModal('room'); }} />
-      <div onClick={() => { updateModal('notification'); }}>Open</div>
+      <div onClick={() => { updateModal('room'); }} className={styles.icon_container}>
+        <FontAwesomeIcon icon={faPlus} className={styles.icon} />
+      </div>
+      {/* <div onClick={() => { updateModal('notification'); }}>Open</div> */}
     </div>
   );
 };
@@ -28,43 +30,10 @@ const Room = (props) => {
 
   return (
     <div onClick={() => { changeRoom(room.id); }} className={styles.room}>
-      <Image className={styles.room_img} src={room.image} alt='' width='50' height='50' />
+      <Image className={styles.room_img} src={room.image} alt='' width='55' height='55' />
       {room.notifications !== 0 && <div className={styles.room_notifications}>{room.notifications}</div>}
     </div>
   );
 };
 
 export default Sidebar;
-
-// import styles from '@/styles/Rooms.module.css';
-// import Image from 'next/image';
-
-// const Rooms = (props) => {
-//   const { rooms } = props;
-
-//   return (
-//     <div className={styles.container}>
-//       <div className={styles.direct}>
-//         <Image className={styles.direct_img} src='/img/kier-in-sight-2iy6ohGsGAc-unsplash.jpg' alt='' width='60' height='60' />
-//       </div>
-//       <div className={styles.line}></div>
-//       <div className={styles.rooms}>
-//         {rooms.map((room, index) => {
-//           return <Room room={room} key={room.id} />
-//         })}
-//       </div>
-//     </div>
-//   );
-// };
-
-// const Room = (props) => {
-//   const { room } = props;
-
-//   return (
-//     <div className={styles.room}>
-//       <Image className={styles.room_img} src={room.image} alt='' width='60' height='60' />
-//     </div>
-//   );
-// };
-
-// export default Rooms;
