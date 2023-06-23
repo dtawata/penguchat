a759cdcbb985854341175024fd7a8c70f12a60b6
Daniel <tawata.daniel@gmail.com>
2023-06-22 22:50:33 -0700

    added toggle icon

--- components/room/Bar.js
+++ components/room/Bar.js
@@ -1,5 +1,7 @@
 import styles from '@/styles/Bar.module.css';
 import { signOut } from 'next-auth/react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBars } from '@fortawesome/free-solid-svg-icons';
 
 const Bar = (props) => {
   const { channel, openSidebar } = props;
@@ -7,7 +9,9 @@ const Bar = (props) => {
   return (
     <div className={styles.container}>
       <div className={styles.flex}>
-        <div onClick={openSidebar} className={styles.toggle}>Toggle</div>
+        <div onClick={openSidebar} className={styles.toggle}>
+          <FontAwesomeIcon icon={faBars} className={styles.icon} />
+        </div>
         <h3 className={styles.title}># {channel.name}</h3>
       </div>
       <div onClick={() => { signOut({ callbackUrl: '/login' }); }} className={styles.sign_out}>Sign Out</div>
